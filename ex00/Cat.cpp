885e53e77a0e49214764bc67401215ecbb582bbb
julcalde <julcalde@student.42heilbronn.de>
2025-08-19 14:24:16 +0200

    wisp the cat is hungry

--- ex00/Cat.cpp
+++ ex00/Cat.cpp
@@ -6,8 +6,39 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 13:34:42 by julcalde          #+#    #+#             */
-/*   Updated: 2025/08/19 13:34:46 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/08/19 14:21:02 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
+
+Cat::Cat()
+{
+	type = "Cat";
+	std::cout << "Cat default constructor called!" << std::endl;
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat destructor called!" << std::endl;
+}
+
+Cat::Cat(const Cat& other) : Animal(other)
+{
+	std::cout << "Cat copy constructor called!" << std::endl;
+}
+
+Cat& Cate::operator=(const Cat& other)
+{
+	if (this != &other)
+	{
+		Animal::operator=(other);
+	}
+	std::cout << "Cat assignment operator called!" << std::endl;
+	return (*this);
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "*WispTheCat hungry noise*" << std::endl;
+}
--- ex00/Cat.hpp
+++ ex00/Cat.hpp
@@ -6,13 +6,24 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 13:35:00 by julcalde          #+#    #+#             */
-/*   Updated: 2025/08/19 13:35:09 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/08/19 13:55:53 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CAT_HPP
 # define CAT_HPP
 
+#include "Animal.hpp"
 
+class Cat : public Animal
+{
+	public:
+		Cat();
+		~Cat();
+		Cat(const Cat& other);
+		Cat& operator=(const Cat& other);
+
+		void makeSound() const;
+};
 
 #endif
